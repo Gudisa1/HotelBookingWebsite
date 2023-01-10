3ab17808a233c6084fae17d798bf0727ff0998f7
Gudisa Gebi <gudisagebi1@gmail.com>
2023-01-10 14:31:01 -0800

    Facility Section Some Edition

--- src/components/facility/Facility.jsx
+++ src/components/facility/Facility.jsx
@@ -1,56 +1,53 @@
 import React from "react";
 import "./facility.css";
-import { RiComputerLine } from "react-icons/ri";
-import { TbParking } from "react-icons/tb";
+import { GrCloudComputer } from "react-icons/gr";
 const Facility = () => {
   return (
-    <section className="section">
-      <div className="facility">
-        <div className="facility-desc">
-          <h2 className="heading-secondary">
-            We do our best facilities provide you
-          </h2>
-          <p className="facility-desc">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy{" "}
-          </p>
-          <a href="#">
-            <button className="btn-contact">Contact Now</button>
-          </a>
+    <section className="section-facility">
+      <div className="facility-des">
+        <h2 className="heading-secondary">
+          We do our best facilities provide you
+        </h2>
+        <p className="facility-para">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy{" "}
+        </p>
+        <a href="#">
+          <button className="btn-contact">Contact Now</button>
+        </a>
+      </div>
+      <div className="facility-fun">
+        <div className="facility-service">
+          <GrCloudComputer className="facility-icon" />
+          <p className="icon-p">Private WorkSpace</p>
         </div>
-        <div className="facility-icons">
-          <div className="facility-icons-layout">
-            <RiComputerLine className="facility-icon" />
-            <p className="icon-description">Private Workspace</p>
-          </div>
-          <div className="facility-icons-layout">
-            <TbParking className="facility-icon" />
-            <p className="icon-description">Parking Area</p>
-          </div>
-          <div className="facility-icons-layout">
-            <RiComputerLine className="facility-icon" />
-            <p className="icon-description">Private Workspace</p>
-          </div>
-          <div className="facility-icons-layout">
-            <RiComputerLine className="facility-icon" />
-            <p className="icon-description">Private Workspace</p>
-          </div>
-          <div className="facility-icons-layout">
-            <RiComputerLine className="facility-icon" />
-            <p className="icon-description">Private Workspace</p>
-          </div>
-          <div className="facility-icons-layout">
-            <RiComputerLine className="facility-icon" />
-            <p className="icon-description">Private Workspace</p>
-          </div>
-          <div className="facility-icons-layout">
-            <RiComputerLine className="facility-icon" />
-            <p className="icon-description">Private Workspace</p>
-          </div>
-          <div className="facility-icons-layout">
-            <RiComputerLine className="facility-icon" />
-            <p className="icon-description">Private Workspace</p>
-          </div>
+        <div className="facility-service">
+          <GrCloudComputer className="facility-icon" />
+          <p className="icon-p">Private WorkSpace</p>
+        </div>
+        <div className="facility-service">
+          <GrCloudComputer className="facility-icon" />
+          <p className="icon-p">Private WorkSpace</p>
+        </div>
+        <div className="facility-service">
+          <GrCloudComputer className="facility-icon" />
+          <p className="icon-p">Private WorkSpace</p>
+        </div>
+        <div className="facility-service">
+          <GrCloudComputer className="facility-icon" />
+          <p className="icon-p">Private WorkSpace</p>
+        </div>
+        <div className="facility-service">
+          <GrCloudComputer className="facility-icon" />
+          <p className="icon-p">Private WorkSpace</p>
+        </div>
+        <div className="facility-service">
+          <GrCloudComputer className="facility-icon" />
+          <p className="icon-p">Private WorkSpace</p>
+        </div>
+        <div className="facility-service">
+          <GrCloudComputer className="facility-icon" />
+          <p className="icon-p">Private WorkSpace</p>
         </div>
       </div>
     </section>
